2f2ed475d2b1b81a0df20fb2b244f630a2bcbfad
github username <github email address>
2019-10-08 22:27:13 -0400

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,35 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementsByTagName('body')[0].addEventListener("click", function(){
+    let classes = event.target.classList;
+    let target = event.target;
+    if (classes.contains('like-glyph')){
+      mimicServerCall()
+      .then(response => {
+        console.log(response);
+        if(target.classList.contains('activated-heart')){
+          target.textContent = EMPTY_HEART;
+          target.classList.remove('activated-heart');
+        }
+        else{
+          target.textContent = FULL_HEART;
+          target.classList.add('activated-heart')
+        }
+      })
+      .catch(error => {
+        let modal = document.getElementById('modal')
+        let h2 = modal.childNodes[0];
+        h2.textContent = error;
+        modal.classList.remove('hidden');
+        setTimeout(() => {modal.classList.add('hidden')}, 5000);
+      })
 
+    }
+
+  })
+});
 
 
 //------------------------------------------------------------------------------
